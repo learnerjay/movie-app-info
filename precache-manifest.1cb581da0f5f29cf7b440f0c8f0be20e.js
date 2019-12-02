f2d9e8fce93fd33808aec3718629399d4b5f689b
learnerjay <jayantmahato2100@gmail.com>
2019-12-02 20:28:41 +0530

    Updates

---
+++ precache-manifest.1cb581da0f5f29cf7b440f0c8f0be20e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bb94efcb6651b311c5ab0f4d5d1ad0",
+    "url": "/Movie-app/index.html"
+  },
+  {
+    "revision": "c85fd2bf5aef198ba399",
+    "url": "/Movie-app/static/css/main.01d67a89.chunk.css"
+  },
+  {
+    "revision": "6b719f5b7436124cb4d0",
+    "url": "/Movie-app/static/js/2.3ac9cb94.chunk.js"
+  },
+  {
+    "revision": "c85fd2bf5aef198ba399",
+    "url": "/Movie-app/static/js/main.7e2074c6.chunk.js"
+  },
+  {
+    "revision": "3d9f05e315514dadc02f",
+    "url": "/Movie-app/static/js/runtime-main.70afea10.js"
+  }
+]);
